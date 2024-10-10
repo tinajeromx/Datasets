077fba5e958158d96ba1f7e4981d6a62dd5ea0e7
Emilio Tinajero <etnjrm@gmail.com>
2024-10-10 17:23:51 -0600

    Update RStudio - Six Sigma Control Charts.R

--- RStudio/CourseraProjects/ControlCharts/RStudio - Six Sigma Control Charts.R
+++ RStudio/CourseraProjects/ControlCharts/RStudio - Six Sigma Control Charts.R
@@ -1,169 +1,306 @@
-# SIX SIGMA - CONTROL CHARTS
-# Coursera Guided Project
-# Instructor - Moses Gummadi
+# 7 Control Charts
+# IMR (XMR) Charts, XbarR-S Charts, P-NP Charts, C-U Charts
+# Following the 4 Western Electric Rules
+# Emilio Tinajero Montes
 
-# This is an R Script file
-# You can open this in R Studio
-# Use # symbol for comments in R Script file
+#Load Libraries
+check = c("data.table", "nortest") %in% rownames(installed.packages())
+if(!check[1]) install.packages("data.table")
+if(!check[2]) install.packages("nortest")
+library(data.table); library(nortest); rm(check)
 
-# R Studio is already downloaded in your Rhyme Desktop
-# To download on your own desktop
-# Use this link https://rstudio.com/products/rstudio/
-# Choose the RStudio Desktop Free Option
+# Import Control Chart Constants (d2,d3,c4,A2,D3,D4,B3,B4)
+ccc = fread("http://pastebin.com/raw/qu2Z56Fa")
 
-# In this Guided Project You Will Learn
-# How To Plot & Interpret Control Charts Using RStudio
+# Control Chart formulae at https://ibb.co/7VFNJ9K, alternatively - https://pasteboard.co/JCU5NJj.jpg
 
-# Approach:
-#    1. Data Types - Variables, Attribute (Type I and II)
-#    2. Control Chart Selecting Based On Data Type
-#    3. Common Cause & Speacial Cause Variability
-#    4. Rules For Stability - Western Electric, Nelson's 
-#    5. Plot & Interpret Variables Charts (XmR / IMR; Xbar-R, Xbar-S)
-#    6. Plot & Interpret Attribute Charts (NP, P, U, C)
-#
-# Data Types Flow Chart
-#
-#    1. "Variables" Data (aka "Continuous" Data)
-#        Criterion 1: Measure, not Count
-#        Criterion 2: Avg of any two values must make sense
-#        Examples: Length, Temperature, Weight, Time, etc
-#     
-#    2. "Attribute" Data Type I (aka "Discrete" Type I)
-#        Criterion 1: Count, not Measure
-#        Criterion 2: Able to count non-occurrences
-#        Examples: # of defective units in a lot of 100
-#                  # of business class seats occupied in the plane
-#
-#    3. "Attribute" Data Type II (aka "Discrete" Type II)
-#        Criterion 1: Count, not Measure
-#        Criterion 2: CANNOT count non-occurrences
-#        Examples: # of scratches on the car
-#                  # of customers in a day
-#                  # of complaints per week
+# I-MR Chart for I (input: time ordered vector)
+XMRChartX <- function(x, label = "x"){
+  n = length(x)
+  mr = abs(x[2:n] - x[1:(n-1)])
+  temp = data.table(index = 1:(n-1), x = x[2:n],mr)
+  avgx = mean(x[2:n]); avgmr = mean(mr)
+  Xlcl = avgx - (3*avgmr/ccc$d2[1])  # LCL = Xbar - 3*MRbar / d2
+  Xucl = avgx + (3*avgmr/ccc$d2[1])  # LCL = Xbar + 3*MRbar / d2
+  deltaz = avgmr/ccc$d2[1]
+  # Plot Charts
+  yupper = avgx + 1.2*(max(temp$x, Xucl) - avgx)
+  ylower = avgx - 1.2*(avgx - min(temp$x, Xlcl))
+  plot(temp$x, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("Individuals Chart (X-MR) of", label))
+  lines(seq(1,n-1), rep(avgx,n-1), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n-1), rep(Xlcl,n-1), col = "red", lwd = 2 )
+  lines(seq(1,n-1), rep(Xucl,n-1), col = "red", lwd = 2 ) 
+  # Create Data for Tests
+  temp = data.table(Points = x, CL = avgx, LCL = Xlcl, UCL = Xucl)
+  temp = TestControlChart(temp)
+  return(temp)
+}
 
-# Control Chart Selection Algorithm
-#   
-#     Variables Data      Individuals           : IMR / XmR Chart
-#                         Groups (2 to 5)       : Xbar - R Chart
-#                         Groups (6+   )        : Xbar - S Chart
-#
-#     Attribute Type-I    Sample Size Const     : NP Chart
-#                         Sample Size Variable  : P Chart
-#
-#     Attribute Type-II   Const. Opportunity    : C Chart
-#                         Variable Opportunity  : U Chart
-#----------------------------------------------------------
+# I-MR Chart for MR (input: time ordered vector)
+XMRChartMR <- function(x, label = "x"){
+  n = length(x)
+  mr = abs(x[2:n] - x[1:(n-1)])
+  temp = data.table(x = x[2:n],mr)
+  avgmr = mean(mr)
+  MRucl = avgmr * ccc$D4[1]
+  # Plot Charts
+  yupper = avgmr + 1.2*(max(temp$mr, MRucl) - avgmr)
+  ylower = avgmr - 1.2*(avgmr - min(temp$mr))
+  plot(temp$mr, type = "o", pch = 20, lwd = 2, ylim = c(0, max(4*avgmr,1.2*max(mr))), ylab = label, main = paste("Moving Range Chart (X-MR) of", label))
+  lines(seq(1,n-1), rep(avgmr,n-1), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n-1), rep(MRucl,n-1), col = "red", lwd = 2 )
+  lines(seq(1,n-1), rep(0,n-1), col = "red", lwd = 2 )
+  # Create Data for Tests
+  temp[, Points := mr]
+  temp[, CL := avgmr]
+  temp[, LCL := 0]
+  temp[, UCL := MRucl]
+  temp = TestControlChart(temp)
+  return(temp)
+}
 
-# Link To Introductory Video
-browseURL("https://youtu.be/N62qUfMksIQ")
+# Xbar-R Chart for Xbar (input: time ordered vector)
+XbarRChartX <- function(x, g, label = "x", group = "g"){
+  if(length(x) != length(g)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  temp = data.table(x = x, g = g)
+  temp = temp[, .(.N, Xbar = mean(x), Range = (max(x)-min(x))), by = g]
+  n = nrow(temp)
+  avgXbar = mean(temp$Xbar); avgRange = mean(temp$Range)
+  temp[, LCL := avgXbar -  ccc$A2[temp$N] * avgRange]
+  temp[, UCL := avgXbar +  ccc$A2[temp$N] * avgRange]
+  # Plot Charts
+  yupper = avgXbar + 1.2*(max(temp$Xbar, temp$UCL) - avgXbar)
+  ylower = avgXbar - 1.2*(avgXbar - min(temp$Xbar, temp$LCL))
+  plot(temp$Xbar, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("Xbar Chart (Xbar-R) of", label, "by", group))
+  lines(seq(1,n), rep(avgXbar,n), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := Xbar]
+  temp[, CL := avgXbar]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# Xbar-R Chart for R (input: time ordered vector)
+XbarRChartR <- function(x, g, label = "x", group = "g"){
+  if(length(x) != length(g)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  temp = data.table(x = x, g = g)
+  temp = temp[, .(.N, Xbar = mean(x), Range = (max(x)-min(x))), by = g]
+  n = nrow(temp)
+  avgXbar = mean(temp$Xbar); avgRange = mean(temp$Range)
+  temp[, LCL := ccc$D3[temp$N]*avgRange]
+  temp[, UCL := ccc$D4[temp$N]*avgRange]
+  # Plot Charts
+  yupper = avgRange + 1.2*(max(temp$Range, temp$UCL) - avgRange)
+  ylower = avgRange - 1.2*(avgRange - min(temp$Range, temp$LCL))
+  plot(temp$Range, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("R Chart (Xbar-R) of", label, "by", group))
+  lines(seq(1,n), rep(avgRange,n), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := Range]
+  temp[, CL := avgRange]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# Xbar-S Chart for Xbar (input: time ordered vector)
+XbarSChartX <- function(x, g, label = "x", group = "g"){
+  if(length(x) != length(g)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  temp = data.table(x = x, g = g)
+  temp = temp[, .(.N, Xbar = mean(x), S = sd(x)), by = g]
+  temp = temp[, S := ifelse(N == 1, 0,S)]
+  n = nrow(temp)
+  avgXbar = mean(temp$Xbar); avgS = mean(temp$S)
+  vLCL = avgXbar -  ccc$A3[temp$N] * avgS
+  vUCL = avgXbar +  ccc$A3[temp$N] * avgS
+  temp[, LCL := vLCL]
+  temp[, UCL := vUCL]
+  # Plot Charts
+  yupper = avgXbar + 1.2*(max(temp$Xbar, temp$UCL) - avgXbar)
+  ylower = avgXbar - 1.2*(avgXbar - min(temp$Xbar, temp$LCL))
+  plot(temp$Xbar, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("Xbar Chart (Xbar-S) of", label, "by", group))
+  lines(seq(1,n), rep(avgXbar,n), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := Xbar]
+  temp[, CL := avgXbar]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# Xbar-S Chart for S (input: time ordered vector)
+XbarSChartS <- function(x, g, label = "x", group = "g"){
+  if(length(x) != length(g)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  temp = data.table(x = x, g = g)
+  temp = temp[, .(.N, Xbar = mean(x), S = sd(x)), by = g]
+  temp = temp[, S := ifelse(N == 1, 0,S)]
+  n = nrow(temp)
+  avgXbar = mean(temp$Xbar); avgS = mean(temp$S)
+  temp[, LCL := ccc$B3[temp$N]*avgS]
+  temp[, UCL := ccc$B4[temp$N]*avgS]
+  # Plot Charts
+  yupper = avgS + 1.2*(max(temp$S, temp$UCL) - avgS)
+  ylower = avgS - 1.2*(avgS - min(temp$S, temp$LCL))
+  plot(temp$S, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("S Chart (Xbar-S) of", label, "by", group))
+  lines(seq(1,n), rep(avgS,n), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := S]
+  temp[, CL := avgS]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# P-Chart (input: Samples, Defects)
+pChart <- function(nn, d, label = "x"){
+  if(length(nn) != length(d)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  if(length(which(d/nn > 1)) > 0) {
+    cat("\n", "Error: more defects than samples:")
+    return(data.table(Sample = nn[which(d/nn > 1)], Defects = d[which(d/nn > 1)]))
+  }
+  temp = data.table(Sample = nn, Defects = d, p = d/nn)
+  n = nrow(temp); pavg = mean(temp$p)
+  temp[, CL := pavg]
+  temp[, LCL := pavg - 3*sqrt(pavg*(1-pavg)/Sample)]
+  temp[, UCL := pavg + 3*sqrt(pavg*(1-pavg)/Sample)]
+  # Plot Charts
+  yupper = pavg + 1.2*(max(temp$p, temp$UCL) - pavg)
+  ylower = pavg - 1.2*(pavg - min(temp$p, temp$LCL))
+  plot(temp$p, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("P-Chart of", label))
+  lines(seq(1,n), rep(pavg,n), col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := p]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# NP-Chart (input: Num Samples (Const), Defects)
+npChart <- function(nn, d, label = "x"){
+  if(max(d) > nn) {
+    cat("\n", "Error: more defects than Sample Size: ","\n")
+    return(d[d > nn])
+  }
+  temp = data.table(NP = d, P = d/nn)
+  n = length(d); pavg = mean(temp$P)
+  temp[, CL := nn*pavg]
+  temp[, LCL := nn*pavg - 3*sqrt(nn*pavg*(1-pavg))]
+  temp[, UCL := nn*pavg + 3*sqrt(nn*pavg*(1-pavg))]
+  # Plot Charts
+  yupper = nn*pavg + 1.2*(max(temp$NP, temp$UCL) - nn*pavg)
+  ylower = nn*pavg - 1.2*(nn*pavg - min(temp$NP, temp$LCL))
+  plot(temp$NP, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("NP-Chart of", label))
+  lines(seq(1,n), temp$CL, col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := NP]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# U-Chart (input: Samples, Defects/Unit)
+uChart <- function(nn, d, label = "x"){
+  if(length(nn) != length(d)) {
+    return(cat("\n", "Input vectors have different lengths."))
+  }
+  temp = data.table(Sample = nn, Defects = d, U = d/nn)
+  n = nrow(temp); uavg = sum(temp$Defects)/sum(temp$Sample)
+  temp[, CL := uavg]
+  temp[, LCL := uavg - 3*sqrt(uavg/nn)]
+  temp[, UCL := uavg + 3*sqrt(uavg/nn)]
+  # Plot Charts
+  yupper = uavg + 1.2*(max(temp$U, temp$UCL) - uavg)
+  ylower = uavg - 1.2*(uavg - min(temp$U, temp$LCL))
+  plot(temp$U, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("U-Chart of", label))
+  lines(seq(1,n), temp$CL, col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := U]
+  temp = TestControlChart(temp)
+  return(temp)
+}
+
+# C-Chart (input: Num Samples (Const), Defects)
+cChart <- function(c, label = "x"){
+  temp = data.table(Defects = c)
+  n = length(c); cavg = mean(temp$Defects)
+  temp[, CL := cavg]
+  temp[, LCL := cavg - 3*sqrt(cavg)]
+  temp[, UCL := cavg + 3*sqrt(cavg)]
+  # Plot Charts
+  yupper = cavg + 1.2*(max(temp$Defects, temp$UCL) - cavg)
+  ylower = cavg - 1.2*(cavg - min(temp$Defects, temp$LCL))
+  plot(temp$Defects, type = "o", pch = 20, lwd = 1.2, ylim = c(ylower,yupper), ylab = label, main = paste("C-Chart of", label))
+  lines(seq(1,n), temp$CL, col = "darkgreen", lwd = 2 )
+  lines(seq(1,n), temp$LCL, col = "red", lwd = 2, type = "s")
+  lines(seq(1,n), temp$UCL, col = "red", lwd = 2, type = "s")  
+  # Create Data for Tests
+  temp[, Points := Defects]
+  temp = TestControlChart(temp)
+  return(temp)
+}
 
 
-#----------------------------------------------------------
-# Load Libraries & Functions
-source("https://pastebin.com/raw/d1UpgBxi") 
-#----------------------------------------------------------
+# Test Western Electric Rules
+TestControlChart <- function(temp){
+  temp[, zone := ifelse((Points-CL) >= 0, (trunc(3*(Points-CL)/(UCL-CL))+1),-1*(trunc(3*(CL-Points)/(CL-LCL))+1))]
+  temp$rule1 = 0; temp$rule2 = 0; temp$rule3 = 0; temp$rule4 = 0
+  temp[, rule1 := ifelse((Points > UCL | Points < LCL),1,0)]
+  ## Rule 2
+  for(i in 1:(nrow(temp)-2) ) {
+    index = which(temp[i:(i+2), zone] > 2) + (i-1)
+    if(length(index) >= 2) {
+      for(j in index) { temp$rule2[j] = temp$rule2[j] + 1 }
+    }
+    index = which(temp[i:(i+2), zone] < -2) + (i-1)
+    if(length(index) >= 2) {
+      for(j in index) { temp$rule2[j] = temp$rule2[j] + 1 }
+    }
+  }
+  temp[,rule2 := ifelse((rule1 == 0 & rule2 > 0),1,0)]
+  ## Rule 3
+  for(i in 1:(nrow(temp)-4) ) {
+    index = which(temp[i:(i+4), zone] > 1) + (i-1)
+    if(length(index) >= 4) {
+      for(j in index) { temp$rule3[j] = temp$rule3[j] + 1 }
+    }
+    index = which(temp[i:(i+4), zone] < -1) + (i-1)
+    if(length(index) >= 4) {
+      for(j in index) { temp$rule3[j] = temp$rule3[j] + 1 }
+    }
+  }
+  temp[,rule3 := ifelse((rule1 == 0 & rule3 > 0),1,0)]
+  ## Rule 4
+  for(i in 1:(nrow(temp)-8) ) {
+    index = which(temp[i:(i+8), zone] > 0) + (i-1)
+    if(length(index) >= 9) temp$rule4[i+8] = 1 
+    index = which(temp[i:(i+8), zone] < 0) + (i-1)
+    if(length(index) >= 9) temp$rule4[i+8] = 1 
+  }
+  cat("\n",
+      "Western Electric Rule 1: There are ",nrow(temp[rule1 > 0]), " violations.", "\n", 
+      "Western Electric Rule 2: There are ",nrow(temp[rule2 > 0]), " violations.", "\n", 
+      "Western Electric Rule 3: There are ",nrow(temp[rule3 > 0]), " violations.", "\n",  
+      "Western Electric Rule 4: There are ",nrow(temp[rule4 > 0]), " violations.",  "\n", "\n") 
+  return(temp)
+}
 
-
-
-#----------------------------------------------------------
-# Task 1 - Plot XMR Chart
-#----------------------------------------------------------
-
-dt = fread("XMR Chart Dataset.csv")
-print(dt)
-
-dt = dt[order(FiscalWeek)]   # Ensure Data Is Time Ordered
-
-x = dt$England
-x = dt$Scotland
-x = dt$England + dt$Scotland +dt$Wales +dt$N_Ireland
-
-out = XMRChartX(x, label = "Sales")    # X in the X-MR Chart
-out = XMRChartMR(x, label = "Sales")   # MR in the X-MR Chart
-
-out  # view control chart data
-
-out[rule1 > 0]   
-
-# Exercise - Plot & Interpret XMR Charts 
-#           (X and MR) for N_Ireland
-
-dt = fread("XMR Chart Dataset.csv")
-
-
-#----------------------------------------------------------
-# Task 2 - Plot & Interpret Xbar-R Chart, Xbar-S Chart
-#----------------------------------------------------------
-
-dt = fread("XbarRS Chart Dataset.csv")
-print(dt)
-
-x = dt$England
-g = dt$Week
-
-out = XbarRChartX(x, g, label = "Sales", group = "Week")  
-out = XbarRChartR(x, g, label = "Sales", group = "Week")  
-
-x = dt$England
-g = dt$Month
-
-out = XbarSChartX(x, g, label = "Sales", group = "Month")  
-out = XbarSChartS(x, g, label = "Sales", group = "Month") 
-
-# Exercise, Plot Xbar-R & Xbar-S for N_Ireland by Week
-
-dt = fread("XbarRS Chart Dataset.csv")
-
-
-#----------------------------------------------------------
-# Task 3 - Plot & Interpret NP Chart and P Chart
-#----------------------------------------------------------
-
-dt = fread("NP & P Chart Dataset.csv")
-View(dt)
-
-# NP Chart (for constant sample size)
-
-mean(dt$Units_Eng)
-
-out = npChart(400, dt$Defects_Eng, label = "Defects")
-
-# What happens when n is different? (Inspect "out")
-out = npChart(300, dt$Defects_Eng, label = "Defects")
-
-# P Chart (for variable sample size)
-
-out = pChart(dt$Units_Eng, dt$Defects_Eng, label = "Defects/ Unit")
-
-# Exercise: NP Chart (Use N = 157) & P Chart for Scotland
-dt = fread("NP & P Chart Dataset.csv")
-
-
-#----------------------------------------------------------
-# Task 4 - Plot & Interpret C Chart and U Chart
-#----------------------------------------------------------
-
-dt = fread("C & U Chart Dataset.csv")
-View(dt)
-
-# C Chart (for constant opportunity size)
-
-out = cChart(dt$PetrolFills, label = "PetrolFills")
-
-# Let's check what happens with NP chart
-out = npChart(1000, dt$PetrolFills)
-
-# U Chart (for variable opportunity size)
-
-out = uChart(dt$Dispensers, dt$PetrolFills, label = "PetrolFills")
-
-
-# Exercise: Plot C & U Charts for DieselFills
-dt = fread("C & U Chart Dataset.csv")
-
-#----------------------------------------------------------
-# End of Guided Project
-# Summary At: https://youtu.be/N62qUfMksIQ
-#----------------------------------------------------------
+cat("\014") 
 
